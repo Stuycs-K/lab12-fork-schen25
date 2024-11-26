50ff8650b362496b07d11a324b596897758e4dab
STACEY_CHEN <schen50@stuy.edu>
2024-11-26 14:50:18 -0500

    added second child

--- main.c
+++ main.c
@@ -8,18 +8,26 @@ int main(){
   pid_t p1;
   pid_t p2;
   p1 = fork();
-  srand(time(NULL));
-  if (p1<0){
+  // p2 = fork();
+  srand(getpid());
+  if (p1<0 || p2<0){
     perror("fork failed\n");
     exit(1);
   }
   else if (!p1){
-    int s = rand()%5;
+    int s = rand()%5+1;
+    printf("%d %dsec\n", getpid(), s);
+    sleep(s);
+    printf("%d finished after %d seconds.\n", getpid(), s);
+  }
+  else if (!p2){
+    int s = rand()%5+1;
     printf("%d %dsec\n", getpid(), s);
     sleep(s);
     printf("%d finished after %d seconds.\n", getpid(), s);
   }
   else{
+    p2 = fork();
     int status;
     wait(&status);
     if (WIFEXITED(status)){
