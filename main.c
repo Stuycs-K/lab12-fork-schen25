debe1a028bfafb4c5265118d830557fdbbc1d3f9
Stacey Chen <schen50@stuy.edu>
2024-11-25 19:55:04 -0500

    set up first child, worked on printing messages

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
-#import <unistd.h>
-#import <stdlib.h>
-#import <stdio.h>
-#import <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <time.h>
 int main(){
+  printf("%d about to create 2 child processes.\n", getpid());
   pid_t p1;
   pid_t p2;
   p1 = fork();
-  if (p1<0) 
+  srand(time(NULL));
+  if (p1<0){
+    perror("fork failed\n");
+    exit(1);
+  }
+  else if (!p1){
+    int s = rand()%5;
+    printf("%d %dsec\n", getpid(), s);
+    sleep(s);
+    printf("%d finished after %d seconds.\n", getpid(), s);
+  }
+  else{
+    int status;
+    wait(&status);
+    if (WIFEXITED(status)){
+      printf("Main Process %d is done. Child %d slept for ___ sec\n", getpid(), p1);
+    }
+  }
 }
